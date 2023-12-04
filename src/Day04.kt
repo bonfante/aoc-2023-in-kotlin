94af8f6cfc0a47f6d48d7d51a2b59d9049aa59e8
Gregório Bonfante <8030979+bonfante@users.noreply.github.com>
2023-12-04 19:37:31 -0300

    Day 4

---
+++ src/Day04.kt
@@ -0,0 +1,53 @@
+import java.io.File
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.system.measureTimeMillis
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return input.map {
+            val (winningNumbers, scratchcardNumbers) = it.split(':')[1].split('|')
+                .map {
+                    it.split(' ')
+                        .map { it.trim() }
+                        .filter { !it.isEmpty() }
+                }
+            val n = winningNumbers.intersect(scratchcardNumbers).size
+
+            if (n == 0) {
+                0
+            } else {
+                Math.pow(2.0, (n - 1).toDouble()).toInt()
+            }
+        }.sum()
+    }
+
+
+    fun part2(input: List<String>): Int {
+        val scratchcards: MutableMap<Int, Int> = mutableMapOf()
+        input.forEach() {
+            val scratchData = it.split(":");
+            val gameId = scratchData.first().removePrefix("Card").trim().toInt()
+            scratchcards[gameId] = scratchcards.getOrElse(gameId) { 0 } + 1;
+            val (winningNumbers, scratchcardNumbers) = scratchData[1].split('|').map {
+                it.split(' ')
+                    .map { it.trim() }
+                    .filter { it.isNotEmpty() }
+            }
+            val n = winningNumbers.intersect(scratchcardNumbers).size
+            (gameId + 1).rangeTo(n + gameId).forEach {
+                val numberOfCopies = scratchcards.getOrElse(gameId) { 1 }
+                val numberOfCards = scratchcards.getOrElse(it) { 0 }
+                scratchcards[it] = numberOfCards + numberOfCopies
+            }
+        }
+        return scratchcards.values.sum()
+    }
+
+
+    check(part1(readInput("day04inputTest")) == 13)
+    check(part1(readInput("day04input")) == 21919)
+    check(part2(readInput("day04inputTest")) == 30)
+    check(part2(readInput("day04input")) == 9881048)
+}
